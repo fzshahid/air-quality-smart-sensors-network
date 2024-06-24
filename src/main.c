bb174035389b5b1e6c0f23b996fd169569de751e
faixan23 <faixan23@gmail.com>
2024-06-24 13:52:13 +0200

    sps 30

--- src/main.c
+++ src/main.c
@@ -14,50 +14,76 @@
 
 #define CCS811_I2C_ADDRESS 0x5A
 
-int main(void)
-{
-        static const struct device *i2c_dev = DEVICE_DT_GET(I2C_NODE);
+#include <zephyr/device.h>
+#include <stdio.h>  // printk
 
-        if (i2c_dev == NULL)
-        {
-                printk("Failed to get I2C device\n");
-                return -1;
+#include "../sensor_SPS30/src/sps30.h";
+
+/**
+ * TO USE CONSOLE OUTPUT (printk) PLEASE ADAPT TO YOUR PLATFORM:
+ * #define printk(...)
+ */
+
+int main(void) {
+    struct sps30_measurement m;
+    int16_t ret;
+
+    /* Initialize I2C bus */
+    sensirion_i2c_init();
+
+    /* Busy loop for initialization, because the main loop does not work without
+     * a sensor.
+     */
+    while (sps30_probe() != 0) {
+        printk("SPS sensor probing failed\n");
+        sensirion_sleep_usec(1000000); /* wait 1s */
+    }
+    printk("SPS sensor probing successful\n");
+
+    uint8_t fw_major;
+    uint8_t fw_minor;
+    ret = sps30_read_firmware_version(&fw_major, &fw_minor);
+    if (ret) {
+        printk("error reading firmware version\n");
+    } else {
+        printk("FW: %u.%u\n", fw_major, fw_minor);
+    }
+
+    char serial_number[SPS30_MAX_SERIAL_LEN];
+    ret = sps30_get_serial(serial_number);
+    if (ret) {
+        printk("error reading serial number\n");
+    } else {
+        printk("Serial Number: %s\n", serial_number);
+    }
+
+    ret = sps30_start_measurement();
+    if (ret < 0)
+        printk("error starting measurement\n");
+    printk("measurements started\n");
+
+    while (1) {
+        sensirion_sleep_usec(SPS30_MEASUREMENT_DURATION_USEC); /* wait 1s */
+        ret = sps30_read_measurement(&m);
+        if (ret < 0) {
+            printk("error reading measurement\n");
+
+        } else {
+            printk("measured values:\n"
+                   "\t%0.2f pm1.0\n"
+                   "\t%0.2f pm2.5\n"
+                   "\t%0.2f pm4.0\n"
+                   "\t%0.2f pm10.0\n"
+                   "\t%0.2f nc0.5\n"
+                   "\t%0.2f nc1.0\n"
+                   "\t%0.2f nc2.5\n"
+                   "\t%0.2f nc4.5\n"
+                   "\t%0.2f nc10.0\n"
+                   "\t%0.2f typical particle size\n\n",
+                   m.mc_1p0, m.mc_2p5, m.mc_4p0, m.mc_10p0, m.nc_0p5, m.nc_1p0,
+                   m.nc_2p5, m.nc_4p0, m.nc_10p0, m.typical_particle_size);
         }
+    }
 
-        printk("I2C device found\n");
-
-        struct ccs811_data ccs811;
-
-        if (ccs811_init(&ccs811, i2c_dev, CCS811_I2C_ADDRESS) != 0)
-        {
-                printk("Failed to initialize CCS811 sensor\n");
-                return -1;
-        }
-
-        printk("CCS811 initialized\n");
-
-        while (1)
-        {
-                uint16_t eco2, tvoc;
-
-                if (ccs811_data_ready(&ccs811))
-                {
-                        if (ccs811_read(&ccs811, &eco2, &tvoc) == 0)
-                        {
-                                printk("eCO2: %d ppm, TVOC: %d ppb\n", eco2, tvoc);
-                        }
-                        else
-                        {
-                                printk("Failed to read CCS811 sensor data\n");
-                        }
-                }
-                else
-                {
-                        printk("CCS811 data not ready :(\n");
-                }
-
-                k_sleep(K_SECONDS(1));
-        }
-
-        return 0;
+    return 0;
 }
